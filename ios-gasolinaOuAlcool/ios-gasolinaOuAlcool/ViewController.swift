fd92838e831267a3304e3e331e2c2ba695393fd3
cesar-ferreira <cesar.ferreira.tn@gmail.com>
2019-11-15 15:56:27 -0300

    adding logic

--- ios-gasolinaOuAlcool/ios-gasolinaOuAlcool/ViewController.swift
+++ ios-gasolinaOuAlcool/ios-gasolinaOuAlcool/ViewController.swift
@@ -10,11 +10,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultadoLegenda: UILabel!
+    @IBOutlet weak var precoAlcoolCampo: UITextField!
+    @IBOutlet weak var precoGasolinaCampo: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func calcularCombustivel(_ sender: Any) {
+    
+        if let precoAlcool = precoAlcoolCampo.text {
+            if let precoGasolina = precoGasolinaCampo.text {
+         
+                let validaCampos = self.validarCampos(precoAlcool: precoAlcool, precoGasolina: precoGasolina)
+                
+                if (validaCampos) {
+                    calcularMelhorPreco(precoAlcool: precoAlcool, precoGasolina: precoGasolina)
+                } else {
+                    resultadoLegenda.text = "Digite os precos para calcular!"
+                }
+            }
+        }
+    }
+    
+    func calcularMelhorPreco(precoAlcool: String, precoGasolina: String) {
+    
+        if let valorAlcool = Double( precoAlcool ) {
+            if let valorGasolina = Double( precoGasolina ) {
+               
+                let resultadoPreco = valorAlcool / valorGasolina
+            
+                if resultadoPreco >= 0.7 {
+                    self.resultadoLegenda.text = "Melhor utilizar Gasolina!"
+                } else {
+                    self.resultadoLegenda.text = "Melhor utilizar Alcool!"
+                }
+            }
+        }
+    }
+    
+    func validarCampos(precoAlcool: String, precoGasolina: String) -> Bool {
+        
+        var camposValidados = true
+        
+        if (precoAlcool.isEmpty || precoGasolina.isEmpty) {
+            camposValidados = false
+        }
+        
+        return camposValidados
+    }
 }
 
